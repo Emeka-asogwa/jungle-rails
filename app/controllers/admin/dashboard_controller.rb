9409024cc8368b1f527124cf381fc38702b6f798
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-08-26 14:10:02 -0400

    ✍🏽 Add the controller dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
-class Admin::DashboardController < ApplicationController
+class Admin::DashboardController < Admin::AuthController
+
+  before_filter :authorize_user
+
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
